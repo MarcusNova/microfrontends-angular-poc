6a17ff5a0202b05dcd1f95028d6be17296b51dfe
Jhonatan Villanova <jvillanova07@gmail.com>
2024-11-05 10:59:51 -0500

    nueva version de angular y ajustes de boton en formulario

--- projects/host/src/app/home/ui/register/register.component.ts
+++ projects/host/src/app/home/ui/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component } from "@angular/core";
-import { FormGroup, FormControl } from "@angular/forms";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 import Swal from "sweetalert2";
-import profileBehavior from "mfProfile/Behavior";
+// import profileBehavior from "mfProfile/Behavior";
 
 @Component({
     selector: 'app-register',
@@ -10,9 +10,9 @@ import profileBehavior from "mfProfile/Behavior";
 })
 export class RegisterComponent {
     registerForm = new FormGroup({
-        name: new FormControl(''),
-        email: new FormControl(''),
-        age: new FormControl(0)
+        name: new FormControl('', [Validators.required]),
+        email: new FormControl('', [Validators.required]),
+        age: new FormControl(0, [Validators.min(1)])
     })
 
     onSubmitHandler() {
@@ -21,14 +21,12 @@ export class RegisterComponent {
             email: this.registerForm.get('email')?.value ?? '',
             age: this.registerForm.get('age')?.value ?? 0
         };
-        if (data.name != '') {
-            profileBehavior.next(data);
-            Swal.fire({
-                title: '¡Bien hecho!',
-                text: 'Te has registrado con éxito.',
-                icon: 'success',
-                confirmButtonColor: '#EE5351'
-            })
-        }
+        // profileBehavior.next(data);
+        // Swal.fire({
+        //     title: '¡Bien hecho!',
+        //     text: 'Te has registrado con éxito.',
+        //     icon: 'success',
+        //     confirmButtonColor: '#EE5351'
+        // })
     }
 }
